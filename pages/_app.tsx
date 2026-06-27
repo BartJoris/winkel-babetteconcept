cc703432225223adf10a4949cdef564cc0b7dd88
Bart Joris <mail@bartjoris.be>
2026-06-27 08:47:09 +0200

    Zonder banner

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,17 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
 import Navigation from "../components/Navigation";
 
+const PAGES_WITHOUT_NAV = ['/foodtruck'];
+
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  const hideNav = PAGES_WITHOUT_NAV.includes(router.pathname);
+
   return (
     <>
-      <Navigation />
+      {!hideNav && <Navigation />}
       <Component {...pageProps} />
     </>
   );
